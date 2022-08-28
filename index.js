c9a40cd459885a2cd076a8a0a31c1484400ab10b
John Okwaro <john.okwaro@moringaschool.com>
2022-08-28 04:46:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  //look through each number in the array
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    // look through the rest of the numbers
+    for (let j = i+1; j < array.length; j++) {
+      // check if the complement is in the rest of the numbers
+      if (array[j] === complement) return true;
+              
+    }
+    
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  Runtime complexity O(n^2)
 */
 
 /* 
   Add your pseudocode here
+
+  iterate throught each number in the array
+  for the current number, identify its complement that adds it upto the target
+  iterate through the numbers in the array
+  check if any number is equal to the complement
+  if so, return true, otherwise return false
 */
 
 /*
